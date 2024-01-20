4656ac5e3080d71d078870222f63a97c1b2c0e3f
David Mojica <davidmojicav@gmail.com>
2024-01-20 10:16:14 -0500

    Logo Asset

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow:{
+        'white': [
+          '0 35px 35px rgba(255, 255, 255, 0.25)',
+          '0 45px 65px rgba(255, 255, 255, 0.15)'
+        ]
+      }
+    },
   },
   plugins: [],
 }
