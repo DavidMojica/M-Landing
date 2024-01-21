8eefb2b7072f185b6c52a41990a8fe2eeb7396bf
David Mojica <davidmojicav@gmail.com>
2024-01-20 20:17:28 -0500

    Atropos Toggler Done

--- app/dom.ts
+++ app/dom.ts
@@ -1,7 +1,21 @@
+//----------------VARIABLES------------------//
 const toggleLight: HTMLInputElement | null = document.getElementById('toggleLight') as HTMLInputElement;
+const aDay: HTMLElement | null = document.getElementById('a-day');
+const aNight: HTMLElement | null = document.getElementById('a-night');
 
+//-----------------FUNCTIONS----------------//
+function aToggler():void{
+    toggleLight.checked? aNight.style.display = 'block' : aNight.style.display = 'none';
+    toggleLight.checked? aDay.style.display = 'none' : aDay.style.display = 'block';
+}
+
+//-------------DOM MANGNAMENT--------------//
 if (toggleLight) {
     toggleLight.addEventListener('change', () => {
         document.querySelector('html')?.classList.toggle('dark', toggleLight.checked);
+        aToggler();
     });
 }
+
+//--------------INIT---------------//
+aToggler();
