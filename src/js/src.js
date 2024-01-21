d54c80f7a23685e5b9232d0b0ac4cd750d566c51
David Mojica <davidmojicav@gmail.com>
2024-01-21 16:45:30 -0500

    Swipers

--- src/js/src.js
+++ src/js/src.js
@@ -1,7 +1,7 @@
 import {Atropos} from '../../node_modules/atropos/atropos.min.mjs';
+import {Swiper} from '../../node_modules/swiper/swiper-bundle.min.mjs';
 
-
-
+// Atropos
 const nightScene =  Atropos({
     el: '.a-night',
     activeOffset: 100,
@@ -10,6 +10,38 @@ const nightScene =  Atropos({
 const dayScene = Atropos({
     el: '.a-day',
     activeOffset: 100,
+});
+
+//Swiper
+var swiper = new Swiper(".Swiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay: 2000
+    },
+    loop: true,
+    parallax: true,
+});
+
+var reverseSwiper = new Swiper(".revSw", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay:{
+        delay: 2000,
+        reverseDirection: true,
+    },
+    loop: true,
+    parallax: true,
 });
 
 
